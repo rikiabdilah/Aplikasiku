2a673aaa11892ad8325220e37c6cec14bb713d6e
rikiabdilah <rikiabdillah2002@gmail.com>
2023-03-15 18:38:12 +0700

    latihankedua

--- app/src/main/java/com/example/aplikasiku/MainActivity.kt
+++ app/src/main/java/com/example/aplikasiku/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.aplikasiku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aplikasiku.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+//class MainActivity : AppCompatActivity() {
+//    private lateinit var binding: ActivityMainBinding
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.testing.text = "Assalamualaikum"
+//
+//
+//    }
+//}
